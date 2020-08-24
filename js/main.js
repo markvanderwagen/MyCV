fabfae9da64e2d97dcc465801cda20293342cd41
markvanderwagen <65869857+markvanderwagen@users.noreply.github.com>
2020-08-24 22:32:25 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+let menuOpen = false;
+
+hamburger.addEventListener("click", () => {
+
+  if(!menuOpen) {
+    hamburger.classList.add('open');
+    navLinks.classList.add('open');
+    menuOpen = true;
+  } else {
+    hamburger.classList.remove('open');
+    navLinks.classList.remove('open');
+    menuOpen = false;
+  }
+  
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
+
+
